6989a6ccefb687db0506b5144055180f0150cc27
spikeyamk <spikeyamk@github.com>
2023-08-27 07:54:06 +0200

    auto git push

---
+++ lib/foo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "foo.hpp"
+
+void print_hello_world() {
+	std::cout << "Hello World!\n";
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
+#include "foo.hpp"
 
 int main(void) {
-    std::cout << "Hello World!\n";
+	print_hello_world();
 }
