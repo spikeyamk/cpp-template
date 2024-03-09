c4bd8236b3db9460de5ff00883018a6be8b96c64
spikeyamk <spikeyamk@github.com>
2024-03-09 13:06:11 +0100

    added a subdirectory

--- lib/foo.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#include "foo.hpp"
-
-void print_hello_world() {
-	std::cout << "Hello World!\n";
-}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-#include "foo.hpp"
+#include "upper.hpp"
+#include "inner/inner.hpp"
 
 int main(void) {
-	print_hello_world();
+	print_hello_world_from_upper();
+	print_hello_world_from_inner();
 }
